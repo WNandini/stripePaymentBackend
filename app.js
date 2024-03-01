ab6b98ac81a62e17dba67eba87e86ddfa0fc353c
WNandini <23nandiniwadekar@gmail.com>
2024-03-01 15:55:31 +0530

    adding url to navigation

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ app.post("/api/create-checkout-session", async(req, res) => {
         payment_method_types: ["card"],
         line_items: lineItems,
         mode: 'payment',
-        success_url: 'http://localhost:3000/success',
-        cancel_url: 'http://localhost:3000/cancel' 
+        success_url: 'https://65e1abc9204b5ab5c37e9e85--jazzy-choux-b29d7b.netlify.app/success',
+        cancel_url: 'https://65e1abc9204b5ab5c37e9e85--jazzy-choux-b29d7b.netlify.app/cancel' 
     })
 
     res.json({id: session.id})
